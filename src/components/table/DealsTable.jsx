3da2e749535cee517ba1f7141aee289d3d09b832
Ramit Prayas <mail.ramit2499@gmail.com>
2026-01-29 16:34:44 +0530

    updated add deals

--- src/components/table/DealsTable.jsx
+++ src/components/table/DealsTable.jsx
@@ -81,7 +81,9 @@ function DealsTable() {
         <select
           value={statusFilter}
           onChange={(e) => setStatusFilter(e.target.value)}
-          className="border rounded-lg px-3 py-2 text-sm w-full sm:w-[180px]"
+          className="border rounded-lg px-3 py-2 text-sm w-full sm:w-[180px]
+             appearance-none bg-white
+             focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           <option value="All">All Status</option>
           <option value="Delivered">Delivered</option>
